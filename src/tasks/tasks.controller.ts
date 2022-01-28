cfb0351e6e1dc31f463ed128f7ac6db43c05c1e4
brunocardosof <bruno.cardosof@gmail.com>
2022-01-28 00:52:07 -0300

    feat: adicionado rotas de update, delete e getbyid

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
@@ -12,8 +20,26 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: 'OPEN' | 'IN_PROGRESS' | 'DONE',
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
   }
 }
