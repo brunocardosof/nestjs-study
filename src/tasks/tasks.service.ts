e91c41661824b6e2b2c91b1f1c698730d03df9ed
brunocardosof <bruno.cardosof@gmail.com>
2022-01-29 00:18:01 -0300

    feat: adicionado exception no servico deleteTask caso ID nao exista

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -15,7 +15,7 @@ export class TasksService {
     const found =  this.tasks.find(task => task.id === id);
 
     if(!!!found) throw new NotFoundException(`Task with ID "${id}" not found`);
-    
+
     return found;
   }
 
@@ -33,7 +33,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks =  this.tasks.filter(task => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks =  this.tasks.filter(task => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: "OPEN" | "IN_PROGRESS" | "DONE"): Task {
