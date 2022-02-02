e05b222c3cff41de5add420dd7fb64d18e9a8eee
brunocardosof <bruno.cardosof@gmail.com>
2022-02-02 00:12:32 -0300

    feat: adicionado biblioteca bcrypt
    adicionado hash na senha para salvar no banco

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,13 +5,20 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import  { genSalt, hash } from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<User | ConflictException | InternalServerErrorException> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.create({ username, password });
+    const salt = await genSalt();
+    const hashedPassword = await hash(password, salt); 
+
+    console.log(salt);
+    console.log(hashedPassword);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       return await this.save(user);
     } catch (error) {
