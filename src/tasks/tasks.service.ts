ded8260197b5a2a22720b57530d8bdd0d4c09e68
brunocardosof <bruno.cardosof@gmail.com>
2022-02-04 00:01:08 -0300

    adicionado restricoes no getbyid e update task status somente pro usuario dono da task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -19,8 +19,8 @@ export class TasksService {
     return this.tasksRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: string): Promise<Task> {
-    const found = await this.tasksRepository.findOne(id);
+  async getTaskById(id: string, user: User): Promise<Task> {
+    const found = await this.tasksRepository.findOne({ where: { id, user } });
 
     if (!!!found) throw new NotFoundException(`Task with ID "${id}" not found`);
 
@@ -38,8 +38,12 @@ export class TasksService {
       throw new NotFoundException(`Task with ID "${id}" not found`);
   }
 
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = await this.getTaskById(id, user);
     task.status = status;
 
     await this.tasksRepository.save(task);
