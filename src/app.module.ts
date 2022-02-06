80e4ca7a1ca0a2cfa6f480f420916e78afe32071
brunocardosof <bruno.cardosof@gmail.com>
2022-02-06 15:50:44 -0300

    config para stage

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`]
+    }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -12,7 +16,7 @@ import { AuthModule } from './auth/auth.module';
       port: 5432,
       username: 'postgres',
       password: 'postgres',
-      database: 'task-management222',
+      database: 'task-management-999999999999',
       autoLoadEntities: true,
       synchronize: true
     }),
